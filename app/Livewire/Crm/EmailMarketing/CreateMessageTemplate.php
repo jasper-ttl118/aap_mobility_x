3f5ab0d586bc4d03fcd0e00acbae6b6dc7aa0cbc
SIR VICTOR AQUINO <aquino.sir.victor.bscs2022@gmail.com>
2025-06-04 17:26:04 +0800

    - Added New Message Template Modal - Added View/Edit Message Template Modal - Added Compose New Email Modal - Added Use Message Template Modal for email - Added Compose New Message Modal for mobile number delivery - Added Use Message Template Modal for mobile

---
+++ app/Livewire/Crm/EmailMarketing/CreateMessageTemplate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Crm\EmailMarketing;
+
+use Livewire\Component;
+
+class CreateMessageTemplate extends Component
+{
+    public function render()
+    {
+        return view('livewire.crm.email-marketing.create-message-template');
+    }
+}
