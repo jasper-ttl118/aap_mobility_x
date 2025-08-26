181e57e53ab3ba603b370530d497a95cab98db88
jasper-ttl118 <jasper.aquino@aap.org.ph>
2025-08-26 20:33:10 +0800

    setup_quick: comply with mysql; dont overwrite composer and node packages set by updating

--- setup_quick.sh
+++ setup_quick.sh
@@ -1,10 +1,10 @@
 #!/bin/env bash
 cp .env.example .env
-touch database/database.sqlite
+# touch database/database.sqlite
 composer clear-cache
 composer install && npm install
-composer clear-cache
-composer update && npm update
+# composer clear-cache
+# composer update && npm update
 
 php artisan key:generate
 echo "Done. Run migration before starting application."
